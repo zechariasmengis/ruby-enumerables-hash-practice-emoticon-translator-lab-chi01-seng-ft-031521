8135d41a6ffa2031f90a11b4dfcc9d4b3ecaf234
Zecharias Mengis <zmengis@gmail.com>
2021-03-02 21:32:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,7 @@ def get_japanese_emoticon(path, english_emoticon)
       return value[:japanese]
     end
   end
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, japanese_emoticon)
