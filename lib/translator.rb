ccf1e0ed6daa6ed00d9027e0265aa88c1fa44f48
Zecharias Mengis <zmengis@gmail.com>
2021-03-02 17:14:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require "yaml"
 require "pry"
 
 def load_library(path)
-  emoticon_translator = YAML.load_file('path')
+  emoticon_translator = YAML.load_file(path)
   emoticon_translator
   binding.pry
 end
