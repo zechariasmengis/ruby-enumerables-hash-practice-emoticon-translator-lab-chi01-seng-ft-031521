abb603efec605123ed8e8f4e89790e2dda130709
Zecharias Mengis <zmengis@gmail.com>
2021-03-02 18:24:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "pry"
 
 def load_library(path)
   emoticon_data = YAML.load_file(path)
-  emoticon_data.each_with_object({}) do |(key, value), my_hash|
+  emoticon_data.each do |(key, value), my_hash|
     my_hash = emoticon_data.values
     binding.pry
   end
