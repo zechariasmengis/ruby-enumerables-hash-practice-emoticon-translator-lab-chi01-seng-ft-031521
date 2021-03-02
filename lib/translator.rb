6bcdf1b805e5476fc777d209e95535742e201602
Zecharias Mengis <zmengis@gmail.com>
2021-03-02 19:46:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,8 @@ def load_library(path)
   emoticon_data = YAML.load_file(path)
   emoticon_data.each do |key, value|
     my_hash[key] = {}
-    my_hash[key][:english] = {}
-    my_hash[key][:japanese] = {}
+    my_hash[key][:english] = value[0]
+    my_hash[key][:japanese] = value[1]
   end
   my_hash
 end
