c98513574b8e9f928d82f4e86622b9beae120ca1
Zecharias Mengis <zmengis@gmail.com>
2021-03-02 21:15:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,6 @@ def get_japanese_emoticon(path, english_emoticon)
     value.each do |key2, value2|
       if english_emoticon == value2
         japanese_tanslation = value[:japanese]
-        binding.pry
       end
     end
   end
